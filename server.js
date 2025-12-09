a1ebb8f29fe53c6497f78257e8737aad42ee5abe
Allen Orozco Lopez <allenorozcro@gmail.com>
2025-12-09 17:55:09 -0500

    added code for cloudflare caching

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static('public'));
 
+// Disable Cloudflare caching for API routes to allow Set-Cookie headers
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  next();
+});
+
 // my session secret for express-session
 app.use(session({
   store: new PgSession({
